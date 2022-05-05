6871203afa68537d030340c34185c141e8be916f
McKay Kleinman <vicrumkleinman@yahoo.com>
2022-05-04 18:32:26 -0600

    added s to url

--- public/script.js
+++ public/script.js
@@ -245,7 +245,7 @@ submitRingBtn.addEventListener("click", (event) => {
       notes: notes.value,
     };
 
-    axios.post("http://momentrings.com/api/neworder", newOrder).then(() => {
+    axios.post("https://momentrings.com/api/neworder", newOrder).then(() => {
       window.location.href = "/home.html";
     });
   });
